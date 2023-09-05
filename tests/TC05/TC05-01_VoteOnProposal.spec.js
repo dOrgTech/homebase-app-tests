f5e984014d2f5a6ff067e5689b863fb1af80293a
w3aayushi <aayushi.m@w3dev.email>
2023-09-05 18:10:53 +0530

    change in TC05

--- tests/TC05/TC05-01_VoteOnProposal.spec.js
+++ tests/TC05/TC05-01_VoteOnProposal.spec.js
@@ -11,7 +11,7 @@ test('Test case 05-01: Vote On Proposal ', async ({ page }) => {
     try {
 
         //To Extend the time of test Execution
-        test.setTimeout(160000);  
+        test.setTimeout(160000);
 
         //Check if cycle status is CREATING
         const cyclestatus = await page.getByText(pageLocators.ProposalCreation.Cycle)
@@ -19,29 +19,36 @@ test('Test case 05-01: Vote On Proposal ', async ({ page }) => {
         await expect(cyclestatus).not.toBeVisible();
 
         //Click on the Active Button
-        await page.getByText(pageLocators.VotingOnChain.active, {exact: true}).click(); 
+        const activebtn = await page.getByText(pageLocators.VotingOnChain.active, { exact: true });
+        const isactive = await expect(activebtn).toBeVisible();
+        if (isactive) {
 
-        await page.waitForTimeout(3000);
+            await activebtn.click();
+            await page.waitForTimeout(3000);
 
-        //Click on the Vote For Support
-        await page.getByText(pageLocators.VotingOnChain.VoteFor).click(); 
+            //Click on the Vote For Support
+            await page.getByText(pageLocators.VotingOnChain.VoteFor).click();
 
-        //Enter an amount
-        await page.type(pageLocators.VotingOnChain.Amount, TezosTestData.VotingOnChain.Amount); 
+            //Enter an amount
+            await page.type(pageLocators.VotingOnChain.Amount, TezosTestData.VotingOnChain.Amount);
 
-        await page.waitForTimeout(5000)
+            await page.waitForTimeout(5000)
 
-        //Click on the Submit
-        await page.getByText(pageLocators.VotingOnChain.Submit).click(); 
+            //Click on the Submit
+            await page.getByText(pageLocators.VotingOnChain.Submit).click();
 
-        //Wait for Submit the Proposal
-        await page.waitForTimeout(10000);
-        
-        //Validate that Confirmation message is shown
-        const validateText = await page.getByText(pageLocators.VotingOnChain.validateText);
-        await expect(validateText).toBeVisible();
+            //Wait for Submit the Proposal
+            await page.waitForTimeout(10000);
 
-        console.log("Vote was casted successfully.");
+            //Validate that Confirmation message is shown
+            const validateText = await page.getByText(pageLocators.VotingOnChain.validateText);
+            await expect(validateText).toBeVisible();
+
+            console.log("Vote was casted successfully.");
+        }
+        else{
+            console.log("No proposal 'ACTIVE' to vote.")
+        }
 
     }
     catch (error) {
@@ -49,6 +56,6 @@ test('Test case 05-01: Vote On Proposal ', async ({ page }) => {
         console.log(error);
     }
 
-  
+
 
 })
