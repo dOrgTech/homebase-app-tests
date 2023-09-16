6578fc4bbed484a95048acfb0560617a49352b80
w3rahul <rahul.p@w3dev.email>
2023-09-16 19:51:55 +0530

    WIP: Test Case - Module TC 05 and more

--- tests/TC07/TC07-01_ProposalExecution.spec.js
+++ tests/TC07/TC07-01_ProposalExecution.spec.js
@@ -3,30 +3,41 @@ const { PreProposal } = require("../CommonFile/PreAction");
 const { pageLocators } = require("../CommonFile/Locator");
 
 test('Test case 1: On-Chain Proposal Execution', async ({ page }) => {
+    test.setTimeout(10 * 60 * 1000); // Extending Test Case timeout to 10 minutes
 
-    test.setTimeout(20 * 60 * 1000); //Extending Test Case timeout to 20 minutes
-
-    await PreProposal(page);   //PreConditions Open URL and Open Mask DAO For Proposal   
+    await PreProposal(page); // PreConditions Open URL and Open Mask DAO For Proposal
 
     try {
+        await page.click(pageLocators.OffChainPoll.cycle); // Click on Running Cycle Status
 
-        await page.click(pageLocators.OffChainPoll.cycle);  // Click on Running Cycle Status
+        let elementFound = false;
+        let retries = 0;
+        const maxRetries = 3; // Set the maximum number of retries as needed
 
-        await page.waitForSelector(pageLocators.ProposalExecution.Execute, { click: true, timeout: 600000 });
+        while (!elementFound && retries < maxRetries) {
+            try {
+                await page.waitForSelector(pageLocators.ProposalExecution.Execute, { timeout: 300000 });
+                await page.click(pageLocators.ProposalExecution.Execute); // Click on Execute Button
+                elementFound = true; // Element found and clicked successfully
+            } catch (error) {
 
-        await page.click(pageLocators.ProposalExecution.Execute);  //Click on Execute Button
+                console.log(`Retry ${retries + 1}: Element not found or not clickable`);  // Handle the error or log it
+                retries++;
+            }
+        }
 
-        await page.waitForTimeout(30000); //Wait for transaction
+        if (elementFound) {
+            await page.waitForTimeout(30000); // Wait for transaction
 
-        const content = await page.content(); //Assume Page
+            const content = await page.content(); // Assume Page
 
-        const isTextVisible = content.includes(pageLocators.ProposalExecution.TransactionText, { visible: true });  //Verify that text visible on the webpage
+            const isTextVisible = content.includes(pageLocators.ProposalExecution.TransactionText, { visible: true }); // Verify that text is visible on the webpage
 
-        console.log(isTextVisible); //Prints True or false for is Text Visible
-
-    }
-    catch (error) {
+            console.log(isTextVisible); // Prints True or false for whether the text is visible
+        } else {
+            console.log(`Error: Element not found or not clickable after ${maxRetries} retries`);
+        }
+    } catch (error) {
         console.log("Error: No Passed- Executable Proposals");
     }
-
-})
+});
